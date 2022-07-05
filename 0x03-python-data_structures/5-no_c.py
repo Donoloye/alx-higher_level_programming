7a6335b5f483de3211b2f5c06b926fe36d7a4643
Joe <damilare2010@yahoo.com>
2022-07-05 02:04:38 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    copy = my_list.copy()
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list.copy()
-    else:
-        copy[idx] = element
-        return copy
+def no_c(my_string):
+    return my_string.translate({ord(c): None for c in "cC"})
