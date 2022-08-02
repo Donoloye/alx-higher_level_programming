a4a4121874e33df250944cf532b994f72ce39961
Donoloye <damilare2010@yahoo.com>
2022-08-01 17:46:12 -0700

    learning python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Inheritance module"""
-
-
-def inherits_from(obj, a_class):
-    """tells whether argA inherits from argB"""
-    return True if isinstance(obj, a_class) and type(obj) != a_class else False
